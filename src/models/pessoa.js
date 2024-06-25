7b2e1d24f735af761a1539765992584a1ccc71f5
Lucas Shinozaki <lucas_shinozaki@hotmail.com>
2024-06-25 17:09:21 -0300

    Refatorando model e controller

--- src/models/pessoa.js
+++ src/models/pessoa.js
@@ -1,40 +1,23 @@
 'use strict';
-/** @type {import('sequelize-cli').Migration} */
-module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('pessoas', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      nome: {
-        type: Sequelize.STRING
-      },
-      email: {
-        type: Sequelize.STRING
-      },
-      cpf: {
-        type: Sequelize.STRING
-      },
-      ativo: {
-        type: Sequelize.BOOLEAN
-      },
-      role: {
-        type: Sequelize.STRING
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('pessoas');
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Pessoa extends Model {
+
+    static associate(models) {
+      // define association here
+    }
   }
+  Pessoa.init({
+    nome: DataTypes.STRING,
+    email: DataTypes.STRING,
+    cpf: DataTypes.STRING,
+    ativo: DataTypes.BOOLEAN,
+    role: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Pessoa',
+  });
+  return Pessoa;
 };
